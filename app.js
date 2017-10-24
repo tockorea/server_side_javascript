2ea0a22d31b38220cc0b0ba23eadc6c49e99da8e
tockorea <lim.chae.eun@gmail.com>
2017-10-24 22:08:37 +0900

    정적 vs 동적, Jade

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 var express = require('express');
 var app = express();
+app.locals.pretty = true;
+app.set('view engine', 'jade');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/template', function(req, res) {
+  res.render('temp', {
+    time: Date(),
+    title: 'Jade'
+  }); 
+});
 
 app.get('/', function(req, res) {
-  res.send('<h1>Welcom to homepage</h1>');
+  res.send('<h1>Hello home page</h1>');
+});
+
+app.get('/dynamic', function(req, res) {
+  var lis = '';
+  for (var i = 0; i < 5; i++) {
+    lis = lis + '<li>coding</li>';
+  }
+  var time = Date();
+  var output = `
+  <!DOCTYPE html>
+  <html>
+
+  <head>
+    <meta charset="utf-8">
+    <title>static html</title>
+  </head>
+
+  <body>
+    Hello, Dynamic!
+    <ul>
+      ${lis}
+    </ul>
+    ${time}
+  </body>
+
+  </html>
+  `;
+  res.send(output);
+});
+
+app.get('/route', function(req, res) {
+  res.send('Hello Router, <img src="/route.png">');
 });
 
 app.get('/login', function(req, res) {
