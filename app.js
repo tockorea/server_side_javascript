9332c4a4c35920856d7c577c0642c69443325ec6
tockorea <lim.chae.eun@gmail.com>
2017-10-26 00:26:29 +0900

    query string, semantic url, post 방식

--- app.js
+++ app.js
@@ -5,11 +5,40 @@ app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
 
+app.get('/form', function(req, res) {
+  res.render('form');
+});
+
+app.get('/form_receiver', function(req, res) {
+  var title = req.query.title;
+  var description = req.query.description;
+  res.send(title + ', ' + description);
+});
+
+app.get('/topic/:id', function(req, res) {
+  var topics = [
+    'JavaScript is ...',
+    'Nodejs is ...',
+    'Express is ...'
+  ];
+  var output = `
+  <a href="/topic/0">JavaScript</a><br>
+  <a href="/topic/1">Nodejs</a><br>
+  <a href="/topic/2">Express</a><br><br>
+  ${topics[req.params.id]}
+  `;
+  res.send(output);
+});
+
+app.get('/topic/:id/:mode', function(req, res) {
+  res.send(req.params.id + ', ' + req.params.mode);
+});
+
 app.get('/template', function(req, res) {
   res.render('temp', {
     time: Date(),
     title: 'Jade'
-  }); 
+  });
 });
 
 app.get('/', function(req, res) {
