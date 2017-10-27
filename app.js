a38dcf35effb49278953129a8246910bb79123c3
tockorea <lim.chae.eun@gmail.com>
2017-10-27 21:40:31 +0900

    body-Parser

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.get('/form', function(req, res) {
   res.render('form');
@@ -12,6 +16,12 @@ app.get('/form', function(req, res) {
 app.get('/form_receiver', function(req, res) {
   var title = req.query.title;
   var description = req.query.description;
+  res.send(title + ', ' + description);
+});
+
+app.post('/form_receiver', function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
   res.send(title + ', ' + description);
 });
 
