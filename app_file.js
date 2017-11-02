76f5d92120f98bfc5658d6bb7bc81d38a8a4f89d
tockorea <lim.chae.eun@gmail.com>
2017-11-02 22:56:36 +0900

    파일 업로드

--- app_file.js
+++ app_file.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({
+  storage: _storage
+});
 var fs = require('fs');
 var app = express();
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+app.use('/user', express.static('uploads'));
 app.locals.pretty = true;
 app.set('views', './views_file');
 app.set('view engine', 'jade');
+app.get('/upload', function(req, res) {
+  res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res) {
+  console.log(req.file);
+  res.send('Uploaded : ' + req.file.filename);
+});
 app.get('/topic/new', function(req, res) {
   fs.readdir('data', function(err, files) {
     if (err) {
