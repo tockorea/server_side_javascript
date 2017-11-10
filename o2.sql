7ca070ce4e15a883cb034c429f571a1b6f72c259
tockorea <lim.chae.eun@gmail.com>
2017-11-10 18:19:36 +0900

    o2 DB 스크립트

---
+++ o2.sql
@@ -0,0 +1,26 @@
+﻿CREATE DATABASE o2 CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+use o2;
+
+CREATE TABLE `topic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `author` varchar(30) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO topic (title, description, author) VALUES('JavaScript','Computer language for web.', 'egoing');
+INSERT INTO topic (title, description, author) VALUES('NPM','Package manager', 'leezche');
+
+CREATE TABLE users ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    authId VARCHAR(50) NOT NULL,
+    username VARCHAR(30), 
+    password VARCHAR(255), 
+    salt VARCHAR(255),
+    displayName VARCHAR(50),
+    email VARCHAR(50), 
+    PRIMARY KEY (id), 
+    UNIQUE (authId)
+) ENGINE = InnoDB;
